aec4ab85cccc45ded59e0d1f1c179b9ba3de96b5
CodyBrewer <codybrewerdev@gmail.com>
2020-04-28 12:18:04 -0600

    fail test for if item ehancement is < 15 written

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,25 @@
-module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
-};
-
-function succeed(item) {
+// Returns item modified according to the rules defined by the client for the enhancement success.
+function success(item) {
   return { ...item };
 }
 
+// Returns item modified according to the rules defined by the client for the enchancement failure.
 function fail(item) {
   return { ...item };
 }
 
+// Repair, returns the item with durability set to 100.
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
   return { ...item };
 }
+
+module.exports = {
+  success,
+  fail,
+  repair,
+  get,
+};
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -5,31 +5,54 @@ const weapons = [
   {
     type: 'sword',
     durability: 50,
-    enhancement: 3,
+    enhancement: 15,
   },
   {
     name: 'bow',
     durability: 60,
-    enhancement: 2,
+    enhancement: 18,
   },
   {
     name: 'lance',
     durability: 100,
-    enhancement: 0,
+    enhancement: 5,
+  },
+  {
+    name: 'axe',
+    durability: 3,
+    enhancement: 3,
   },
 ];
 
-describe('repair', () => {
-  const { repair } = enhancer;
 
-  it('takes in an item and returns the item back with durability property of 100', () =>{
-    // arrange
-    const expectedResult = 100;
+describe('enhancer', () => {
 
-    // act
-    const actualResults = weapons.map((weapon) => repair(weapon));
+  describe('repair', () => {
+    const { repair } = enhancer;
 
-    // assess
-    actualResults.forEach((actualResult) => expect(actualResult.durability).toBe(expectedResult));
+    it('takes in an item and returns the item back with durability property of 100', () => {
+      // arrange
+      const expectedResult = 100;
+
+      // act
+      const actualResults = weapons.map((weapon) => repair(weapon));
+
+      // assess
+      actualResults.forEach((actualResult) => expect(actualResult.durability).toBe(expectedResult));
+    });
+  });
+  describe('fail', () => {
+    const { fail } = enhancer;
+
+    it('takes in an item and if item\'s enhancement < 15 durability of item is decreased by 5 or 0 whichever is greater', () => {
+      // arrange
+      const expectedResults = [50, 60, 95, 0];
+
+      // act
+      const actualResults = weapons.map((weapon) => fail(weapon));
+
+      // assess
+      actualResults.forEach((result, idx) => expect(result.durability).toBe(expectedResults[idx]));
+    });
   });
 });

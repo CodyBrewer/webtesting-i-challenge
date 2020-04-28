4f452d3fd905eada984ed97bfa78d8109b60cc25
CodyBrewer <codybrewerdev@gmail.com>
2020-04-28 11:46:47 -0600

    test written for repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,35 @@
+// Remember AAA - Arrange, Act, Assess
 const enhancer = require('./enhancer.js');
-// test away!
+
+const weapons = [
+  {
+    type: 'sword',
+    durability: 50,
+    enhancement: 3,
+  },
+  {
+    name: 'bow',
+    durability: 60,
+    enhancement: 2,
+  },
+  {
+    name: 'lance',
+    durability: 100,
+    enhancement: 0,
+  },
+];
+
+describe('repair', () => {
+  const { repair } = enhancer;
+
+  it('takes in an item and returns the item back with durability property of 100', () =>{
+    // arrange
+    const expectedResult = 100;
+
+    // act
+    const actualResults = weapons.map((weapon) => repair(weapon));
+
+    // assess
+    actualResults.forEach((actualResult) => expect(actualResult.durability).toBe(expectedResult));
+  });
+});

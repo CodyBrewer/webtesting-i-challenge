a2ddd5510074b45dae54999732ed52ed71951320
CodyBrewer <codybrewerdev@gmail.com>
2020-04-29 11:20:37 -0600

    fix typo in dbConfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
-const db = knex(knexConfig.devlopment);
+const db = knex(knexConfig.development);
 
 module.exports = db;

10f3f8007a582f5506779d4eef60e73957afcb26
CodyBrewer <codybrewerdev@gmail.com>
2020-04-29 10:28:08 -0600

    get weapons/ endpoint

--- api/weapons/router.js
+++ api/weapons/router.js
@@ -2,7 +2,18 @@ const router = require('express').Router();
 const Weapons = require('./models.js');
 
 router.get('/status', (req, res) => {
-  res.send({ weapons: 'up'});
+  res.send({ weapons: 'up' });
+});
+
+router.get('/', (req, res) => {
+  Weapons.get()
+    .then((weapons) => {
+      if (weapons) res.send(weapons);
+      else res.status(400).json({ message: 'no weapons in inventory' });
+    })
+    .catch((err) =>{
+      res.status(500).json({ message: 'server error', error: err.message });
+    });
 });
 
 module.exports = router;

eab3e1a47cec6d950da782f6c2df1c9a56cf48b1
CodyBrewer <codybrewerdev@gmail.com>
2020-04-29 11:20:57 -0600

    add api/weapons router with / and /status endpoint working

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const weaponsRouter = require('./weapons/router.js');
 
 const app = express();
 
@@ -10,6 +10,10 @@ app.use(helmet());
 app.use(express.json());
 app.use(cors());
 
+// Routing
+app.use('/api/weapons', weaponsRouter);
+
+// API Status Endpoint
 app.get('/api/status', (req, res) => {
   res.send({ api: 'up' });
 });

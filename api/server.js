de3f52a514bb7cb1f14b3d7909403d6f779fdf04
CodyBrewer <codybrewerdev@gmail.com>
2020-04-29 11:16:55 -0600

    add /api/status endpoint

--- api/server.js
+++ api/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+
 const app = express();
 
 // Global Middleware
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
+
+app.get('/api/status', (req, res) => {
+  res.send({ api: 'up' });
+});
 
 module.exports = app;

ea4a5341e533cfcdd905d255477913ee11447c69
CodyBrewer <codybrewerdev@gmail.com>
2020-04-29 11:14:57 -0600

    add cors, helmet, express.json middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 
-module.exports = app;
+// Global Middleware
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
 
+module.exports = app;
